f34dc0954aefb5fd90699e7c3498634fe0e6d8ea
thiagoluann <moreirat064@gmail.com>
2023-07-05 08:55:13 -0300

    att

---
+++ mega.java
@@ -0,0 +1,20 @@
+/*13. Escreva um programa para imprimir uma versão aproximada de um cartão da Mega-Sena
+(somente com os números, respeitando o número de linhas e a distribuição dos números
+nas linhas). */
+
+import java.util.Scanner;
+
+public class mega {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\t     NUMEROS DA MEGA-SENA");
+        System.out.println("\t01 02 03 04 05 06 07 08 09 10\n\t11 12 13 14 15 16 17 18 19 20\n\t21 22 23 24 25 26 27 28 29 30\n\t31 32 33 34 35 36 37 38 39 40\n\t41 42 43 44 45 46 47 48 49 50\n\t51 52 53 54 55 56 57 58 59 60");
+        scanner.close();
+    }
+}
+
+/*Só fiz isso, pois não consegui compreender direito. Estou em dúvida se é para pedir para
+o usuário escolher alguns numeros e depois utilizar uma função para escolher numeros aleatorios
+e informar quantos números o usuário acetou e se ele ganhou ou não.
+*/
