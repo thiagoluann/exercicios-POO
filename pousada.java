f885865323fc4268bfdc927fbb3773099620c316
thiagoluann <moreirat064@gmail.com>
2023-07-10 15:41:33 -0300

    att

---
+++ pousada.java
@@ -0,0 +1,9 @@
+public class pousada {
+    public static void main(String[] args) {
+        int quartosDisponiveis, numQuarto;
+        double preco;
+        String nomeCliente, nomeDono, nomePousada;
+    }
+
+    
+}
